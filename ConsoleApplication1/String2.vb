93a8c408316d01dafd4f2a62535444f35dbb8ea0
Neeraj55#5 <thakurfazer55@gmail.com>
2017-02-15 14:52:00 +0530

    Add Formatting Functions

--- ConsoleApplication1/String2.vb
+++ ConsoleApplication1/String2.vb
@@ -1,7 +1,12 @@
 ﻿Module String2
     Sub main()
+        'Use of "&" sign
         Dim age As Integer = 19
         Console.WriteLine("Neeraj's age is " & age)
         Console.ReadKey()
+        'Formatting Functions in VB
+        MsgBox(FormatNumber(1834))
+        MsgBox(FormatCurrency(5000))
+        MsgBox(FormatPercent(46))
     End Sub
 End Module
