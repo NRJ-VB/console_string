91a1f69598343e1a1ccbabb12469d28a5f741706
Neeraj55#5 <thakurfazer55@gmail.com>
2017-02-16 15:05:22 +0530

    Elseif Conditional Statements

---
+++ ConsoleApplication1/ConditionalStatements.vb
@@ -0,0 +1,17 @@
+﻿Module ConditionalStatements
+    'Elseif Example
+    Sub main()
+        Console.WriteLine("Enter ur Marks :")
+        Dim num = Console.ReadLine()
+        If (num > 90) Then
+            Console.WriteLine("Your grade is A")
+        ElseIf ((num < 90) And (num >= 80)) Then
+            Console.WriteLine("Your grade is B")
+        ElseIf ((num < 80) And (num >= 70)) Then
+            Console.WriteLine("Your grade is C")
+        ElseIf (num < 70) Then
+            Console.WriteLine("You iz fail !!!")
+        End If
+        Console.ReadKey()
+    End Sub
+End Module
