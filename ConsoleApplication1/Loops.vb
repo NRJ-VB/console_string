f1b2eaa7a515d178b76bb4edf3417195348c8716
Neeraj55#5 <thakurfazer55@gmail.com>
2017-02-23 15:12:19 +0530

    Add for Loops

--- ConsoleApplication1/Loops.vb
+++ ConsoleApplication1/Loops.vb
@@ -18,10 +18,20 @@
         Loop
         Console.ReadKey()
         'For Next Loop
-        Console.WriteLine("-----------------------For Loop-----------------------")
-        For x = 1 To 10 Step 3
-            Console.WriteLine("Value of X :{0}", x)
-        Next x
+        Console.WriteLine("-----------------------For Next-----------------------")
+        For x = 1 To 20
+            Console.WriteLine("Value of x ={0}", x)
+        Next
         Console.ReadKey()
+        'For Each Loop
+        Dim myarray() As Integer = {1, 3, 5, 6}
+        Dim myvalue As Integer
+        Console.WriteLine("-----------------------For Each----------------------")
+        For Each myvalue In myarray
+            Console.WriteLine(myvalue)
+        Next
+        Console.ReadKey()
+
+
     End Sub
 End Module
